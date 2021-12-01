ebba7863848de428e50a36e14a5be03ebe7de7a1
Lee Richardson <lee.richardson@vonage.com>
2021-12-01 17:05:23 +0000

    Day 1 commit

---
+++ Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿var day1 = new Day1();
+var measurements = File.ReadAllLines("input/simple_input.txt").Select(i => int.Parse(i));
+
+// 
+// Part One
+// 
+Console.WriteLine("Part 1 Result: " + day1.Part1(measurements.ToArray()));
+
+// 
+// Part Two
+// 
+Console.WriteLine("Part 1 Result: " + day1.Part2(measurements.ToArray()));
