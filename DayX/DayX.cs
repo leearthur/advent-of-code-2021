b613f715ced8ba131ee508162e51d2d1a33c2c52
Lee Richardson <lee.richardson@vonage.com>
2021-12-08 09:21:21 +0000

    Added DayX template

---
+++ DayX/DayX.cs
@@ -0,0 +1,12 @@
+public class DayX
+{
+    public long Part1(string[] input)
+    {
+        return 0;
+    }
+
+    public long Part2(string[] input)
+    {
+        return 0;
+    }
+}
---
+++ DayX/Program.cs
@@ -0,0 +1,12 @@
+﻿var executor = new DayX();
+var input = File.ReadAllLines("input/input_test.txt");
+
+// 
+// Part One
+// 
+Console.WriteLine("Part 1 Result: " + executor.Part1(input));
+
+// 
+// Part Two
+// 
+Console.WriteLine("Part 2 Result: " + executor.Part2(input));
